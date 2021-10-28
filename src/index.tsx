41f624ef924fe4cd6d19dfdb4ac8df5270175ec8
aniket <aniket.umbc@gmail.com>
2021-10-28 12:28:23 +0530

    error handle for iframe

--- src/index.tsx
+++ src/index.tsx
@@ -8,7 +8,6 @@ const App = () => {
   const ref = useRef<any>();
   const iframeRef = useRef<any>();
   const [input, setInput] = useState('');
-  const [code, setCode] = useState('');
 
   const startService = async () => {
     ref.current = await esbuild.startService({
@@ -25,6 +24,8 @@ const App = () => {
     if (!ref.current) {
       return;
     }
+
+    iframeRef.current.srcdoc = html;
 
     const result = await ref.current.build({
       entryPoints: ['index.js'],
@@ -53,7 +54,14 @@ const App = () => {
   <div id="root"></div>
   <script>
   window.addEventListener('message',(event) => {
-    eval(event.data);
+    try{
+      eval(event.data);
+    }catch(err){
+      const root = document.querySelector('#root');
+      root.innerHTML = '<div style="color:red;"> <h4>Runtime Error</h4>' + err + '</div>'
+      console.error(err);
+    }
+   
   },false);
   </script>
   </body>
@@ -69,9 +77,13 @@ const App = () => {
       <div>
         <button onClick={codeSubmitCode}>Submit Code</button>
       </div>
-      <pre>{code}</pre>
 
-      <iframe ref={iframeRef} sandbox='allow-scripts' srcDoc={html} />
+      <iframe
+        ref={iframeRef}
+        sandbox='allow-scripts'
+        srcDoc={html}
+        title='codeFrame'
+      />
     </div>
   );
 };
